32f573dcf7eddaf2fae9f3c3f8275d82c42b5f20
Jacqueline <jcasalia@yahoo.com.br>
2025-01-22 15:22:49 -0300

    Domain model

--- src/main/java/com/casalitech/dslist/controller/GameController.java
+++ src/main/java/com/casalitech/dslist/controller/GameController.java
@@ -2,22 +2,35 @@ package com.casalitech.dslist.controller;
 
 import com.casalitech.dslist.dto.GameMinDTO;
 import com.casalitech.dslist.entities.Game;
+import com.casalitech.dslist.repositores.GameRepository;
 import com.casalitech.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/games")
 public class GameController {
 @Autowired
 private GameService gameService;
+@Autowired
+private GameRepository repository;
 @GetMapping
 public List<GameMinDTO> findAll(){
     List<GameMinDTO>games=gameService.findAll();
     return games;
 }
+
+    @GetMapping("/{id}")
+    public Optional<Game> detalhar(@PathVariable Long id){
+        return repository.findById(id);
+
+
+    }
+
 }
